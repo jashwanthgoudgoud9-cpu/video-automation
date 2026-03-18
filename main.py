e9a2e1380257973cb0a7366d55358723ef4cab01
jashwanthgoudgoud9-cpu <jashwanthgoudgoud9@gmail.com>
2026-03-18 19:23:27 +0530

    Replace video generation with test file creation
    Removed video creation code and added a test file creation.

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import os
-from moviepy.editor import ColorClip
 
+# create output folder
 os.makedirs("output", exist_ok=True)
 
-# Create simple color video (NO ERROR)
-clip = ColorClip(size=(1280,720), color=(0, 0, 0))
-clip = clip.set_duration(5)
-
-clip.write_videofile("output/video.mp4", fps=24)
+# create simple test file
+with open("output/result.txt", "w") as f:
+    f.write("SUCCESS - Automation is working perfectly!")
