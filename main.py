ae0cc8f03416c1b14a9c2f9979035dcea22c0ebe
jashwanthgoudgoud9-cpu <jashwanthgoudgoud9@gmail.com>
2026-03-18 20:37:16 +0530

    Update video color and duration in main.py
    Changed video color from red to blue and duration from 5 to 3 seconds.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import os
-from moviepy.editor import ColorClip
+from moviepy.video.VideoClip import ColorClip
 
+# create output folder
 os.makedirs("output", exist_ok=True)
 
-clip = ColorClip(size=(1280,720), color=(255, 0, 0))
-clip = clip.set_duration(5)
+# create simple video
+clip = ColorClip(size=(1280, 720), color=(0, 0, 255))
+clip = clip.set_duration(3)
 
+# export video
 clip.write_videofile("output/video.mp4", fps=24)
