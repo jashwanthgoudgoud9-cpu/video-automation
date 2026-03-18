64876637bef04148fa090641077dfb03142f41d3
jashwanthgoudgoud9-cpu <jashwanthgoudgoud9@gmail.com>
2026-03-18 13:54:16 +0530

    Replace TextClip with ColorClip for video creation

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import os
-from moviepy.editor import TextClip, CompositeVideoClip
+from moviepy.editor import ColorClip
 
 os.makedirs("output", exist_ok=True)
 
-# Create simple video
-clip = TextClip("Your Automation Working 🚀", fontsize=70, size=(1280,720))
+# Create simple color video (NO ERROR)
+clip = ColorClip(size=(1280,720), color=(0, 0, 0))
 clip = clip.set_duration(5)
 
-video = CompositeVideoClip([clip])
-video.write_videofile("output/video.mp4", fps=24)
+clip.write_videofile("output/video.mp4", fps=24)
